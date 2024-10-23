348f976787cddaa4b0372a7b5eab09bd5d6f2c8f
Beyz61 <6trabzn1@gmail.com>
2024-10-23 18:14:59 +0200

    Added feed Screen

--- lib/feedscreen/feed_screen.dart
+++ lib/feedscreen/feed_screen.dart
@@ -1,12 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:foodie_screen/config/colors.dart';
+import 'package:foodie_screen/repositories/food_data.dart';
+import 'package:foodie_screen/widget/food_container_widget.dart';
 
 class FeedScreen extends StatelessWidget {
   const FeedScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
+    return Scaffold(
+      appBar: AppBar(
+        
+        // title: Text ("Foodie"),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              backroundColor2, 
+              backroundColor1,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+
+      child: Padding(
+            padding: const EdgeInsets.only(left: 0),
+            child: SizedBox(
+              width: 400,
+              height: 900,
+              child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                itemCount: foodRecipe.length,
+                
+                itemBuilder: (context, index) {
+                  final foodItem = foodRecipe[index];
+                  return FoodContainerWidget(
+                    onTap: () {
+                      // showModalBottomSheet(
+                      //   context: context,
+                      //   isScrollControlled: true,
+                      //   builder: (context) =>
+                      //       DrawerScreen(FoodItem: foodRecipe[index]),
+                      // );
+                    }, 
+                    foodRecipe: foodRecipe[index], foodItem: foodItem,
+            );
+            },
+          ),
+        ),
+      ),
+      ),
     );
   }
 }
