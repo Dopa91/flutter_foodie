82525171f2cfc5e21aea609490ac50ff2b424e72
Beyz61 <6trabzn1@gmail.com>
2024-10-23 18:16:50 +0200

    Added Items Class

---
+++ lib/models/food_item_class.dart
@@ -0,0 +1,24 @@
+class FoodItem {
+  String imagePath;
+  String imageTitle;
+  String imageSubTime;
+  String imagePrice;
+  String imageFav;
+  String iconClock;
+  // String imageDescription;
+  // String imageIngredients;
+  // String imageReviews;
+
+  FoodItem({
+    required this.imagePath,
+    required this.imageTitle,
+    required this.imageSubTime,
+    required this.imagePrice,
+    required this.imageFav,
+    required this.iconClock,
+
+    // required this.imageDescription,
+    // required this.imageIngredients,
+    // required this.imageReviews,
+  });
+}
