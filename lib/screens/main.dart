288650c542fe42a4c5b14f043bcca9bee8c09555
Beyz61 <6trabzn1@gmail.com>
2024-10-23 18:11:30 +0200

    Added main

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:foodie_screen/config/themes.dart';
 import 'package:foodie_screen/mainscreen/main_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const FeedScreen());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class FeedScreen extends StatelessWidget {
+  const FeedScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Foodie",
-      theme: ThemeData(
-          scaffoldBackgroundColor: Colors.transparent,
-          fontFamily: "SFProDisplay"),
+      theme: myTheme,
       home: const MainScreen(),
     );
   }
