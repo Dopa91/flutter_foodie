169f6c3e5d6de4eca90843968012dbaa99fac45d
Beyz61 <6trabzn1@gmail.com>
2024-10-24 15:54:24 +0200

    Added Navigation Bar

--- lib/feedscreen/recipe_screen.dart
+++ lib/feedscreen/recipe_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:foodie_screen/config/colors.dart';
+import 'package:foodie_screen/widget/recipe_container_widget.dart';
 
 class RecipeScreen extends StatelessWidget {
   final String foodItem;
@@ -10,18 +11,38 @@ class RecipeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [ 
-            backroundColor2,
-            backroundColor1
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
+      body: SafeArea(
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [ 
+              backroundColor2,
+              backroundColor1
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
           ),
-        )
+          child: Padding(
+            padding: const EdgeInsets.all(16.0), 
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  foodItem, 
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const RecipeContainer(), 
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
-  }
+}
