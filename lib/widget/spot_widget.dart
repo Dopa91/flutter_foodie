625fe12c5b0a0fc198f9530ed614dc3c7fd70040
Beyz61 <6trabzn1@gmail.com>
2024-10-27 00:57:16 +0200

    Added Spot widget for Spot Screen

---
+++ lib/widget/spot_widget.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class SpotWidget extends StatelessWidget {
+    SpotWidget({
+    super.key, required this.text, required this.picture
+  });
+
+String text;
+String picture;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+    alignment: Alignment.bottomCenter,
+    children: [
+      ClipRRect(
+        borderRadius: BorderRadius.circular(12),
+        child: Image.asset(
+          picture,
+          height: 110,
+          fit: BoxFit.cover,
+          width: 160,
+        ),
+      ),
+      Container(
+        width: 180,
+        height: 50,
+        decoration: BoxDecoration(
+        color: Colors.black.withOpacity(0.4),
+        borderRadius: BorderRadius.circular(8),
+        ),
+        alignment: Alignment.center,
+        child: Text(
+          text,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        ),
+    ],
+   );
+  }
+}
