30345c9d38f0be7f552a5cf6982f5eafa2af87a9
Beyz61 <6trabzn1@gmail.com>
2024-10-27 05:56:43 +0100

    added Fav Container

---
+++ lib/widget/fav_container widget.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+class FavContainer extends StatelessWidget {
+  final Function()? onTap;
+  final String text;
+  final String picture1; 
+  final String picture2; 
+  final String picture3; 
+  final String picture4; 
+
+  const FavContainer({
+    super.key,
+    required this.onTap,
+    required this.text,
+    required this.picture1,
+    required this.picture2,
+    required this.picture3,
+    required this.picture4,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Center(
+          child: Container(
+            height: 190,
+            width: 150,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(200, 233, 189, 149).withOpacity(0.8),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              children: [
+                const SizedBox(height: 20), 
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      width: 60,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                          image: AssetImage(picture1), 
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 60,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                          image: AssetImage(picture2), 
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10), 
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      width: 60,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                          image: AssetImage(picture3), 
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 60,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                          image: AssetImage(picture4), 
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Text(
+                    text,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic,
+                      fontFamily: "SFProDisplay",
+                      color: Color.fromARGB(255, 255, 249, 249),            
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
