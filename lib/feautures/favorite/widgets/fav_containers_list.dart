d403169ef75e3308989373dd11f168acea4e337c
Beyz61 <6trabzn1@gmail.com>
2024-12-05 21:18:48 +0100

    Add initial favorite collections to FavContainerList

---
+++ lib/feautures/favorite/widgets/fav_containers_list.dart
@@ -0,0 +1,53 @@
+import 'package:foodie_screen/feautures/favorite/models/fav_collection_item.dart';
+
+List<FavCollection> favCollectionsList = [
+ FavCollection (
+    recipes: [],
+    collectionName: "Alle Rezepte",
+    image1: "assets/images/pizza.png",
+    image2: "assets/images/baklava.png",
+    image3: "assets/images/falafelhumus.png",
+    image4: "assets/images/chickebackround.png",
+  ),
+  FavCollection(
+    recipes: [],
+    collectionName: "Alle Rezepte",
+    image1: "assets/images/asiaaa.png",
+    image2: "assets/images/asiasuhi.png",
+    image3: "assets/images/fühlingsrolle.png",
+    image4: "assets/images/asiaaa.png",
+  ),
+  FavCollection(
+    recipes: [],
+    collectionName: "Besuch",
+    image1: "assets/images/taco.png",
+    image2: "assets/images/suppe.png",
+    image3: "assets/images/fleisch.png",
+    image4: "assets/images/steak.png",
+  ),
+  FavCollection(
+    recipes: [],
+    collectionName: "Nachtisch",
+    image1: "assets/images/subörek.png",
+    image2: "assets/images/soufflé.png",
+    image3: "assets/images/gratin.png",
+    image4: "assets/images/backroundpanck.png",
+  ),
+   FavCollection(
+    recipes: [],
+    collectionName: "BlaBla",
+    image1: "assets/images/subörek.png",
+    image2: "assets/images/soufflé.png",
+    image3: "assets/images/gratin.png",
+    image4: "assets/images/backroundpanck.png",
+  ),
+
+  // DisplayFavContainer(
+  //   recipes: [],
+  //   collectionName: "Alle Rezepte",
+  //   image1: "",
+  //   image2: "",
+  //   image3: "",
+  //   image4: "",
+  // ),
+];
