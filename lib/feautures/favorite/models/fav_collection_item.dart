4ca50a9eaac9374eb60a43b5698f4cc8cbc63f84
Beyz61 <6trabzn1@gmail.com>
2024-12-05 21:16:38 +0100

    Added FavCollection

--- lib/feautures/favorite/models/fav_collection_item.dart
+++ lib/feautures/favorite/models/fav_collection_item.dart
@@ -3,8 +3,17 @@ import 'package:foodie_screen/feautures/feed/models/food_item.dart';
 class FavCollection {
   String collectionName;
   List<FoodItem> recipes;
+  final String image1;
+  final String image2;
+  final String image3;
+  final String image4;
 
-  FavCollection({required this.collectionName, required this.recipes});
+  FavCollection({
+    required this.collectionName,
+    required this.recipes,
+    required this.image1, 
+    required this.image2, 
+    required this.image3, 
+    required this.image4, 
+  });
 }
-
-
