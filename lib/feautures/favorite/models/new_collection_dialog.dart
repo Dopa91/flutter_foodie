2cad177ac25830fc5ec61510819d5c07da4b9321
Beyz61 <6trabzn1@gmail.com>
2024-12-05 21:17:00 +0100

    Add functionality to create new favorite collections

--- lib/feautures/favorite/models/new_collection_dialog.dart
+++ lib/feautures/favorite/models/new_collection_dialog.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:foodie_screen/feautures/favorite/models/fav_collection_item.dart';
+import 'package:foodie_screen/feautures/favorite/widgets/fav_containers_list.dart';
 
 void showNewCollection(BuildContext context) {
+  
     final TextEditingController controller = TextEditingController();
     
     showDialog(
@@ -42,16 +45,29 @@ void showNewCollection(BuildContext context) {
             ),
             TextButton(
               onPressed: () {
-                Navigator.of(context).pop();  
-              },
-              child: const Icon(
-                Icons.check,
-                color: Color.fromARGB(255, 56, 41, 36),
-                size: 30,
-              ),
-            ),
-          ],
+            String collectionName = controller.text; // der name der Kollektion
+                    if (collectionName.isNotEmpty) {
+            favCollectionsList.add(
+           FavCollection(
+            collectionName: collectionName,
+            recipes: [], // kommt später, noch keine Rezepte
+            image1: " ", 
+            image2: " ",
+            image3: " ",
+            image4: "",
+          ),
         );
-      },
-    );
-  }
+      }
+          Navigator.of(context).pop();
+     },
+          child: const Icon(
+           Icons.check,
+           color: Color.fromARGB(255, 56, 41, 36),
+           size: 30,
+           ),
+          ),
+         ],
+      );
+    },
+  );
+}
