f53d587d7ca473228d045a35cac7738b15ef24ca
Beyz61 <6trabzn1@gmail.com>
2024-12-16 09:55:30 +0100

    Added Fitabase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:foodie_screen/config/themes.dart';
 import 'package:foodie_screen/data/repository/database_repository.dart';
 import 'package:foodie_screen/data/repository/mock_database.dart';
 import 'package:foodie_screen/feautures/authentification/screens/main_screen.dart';
+import 'package:foodie_screen/firebase_options.dart';
+
 
 void main() async {
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+options: DefaultFirebaseOptions.currentPlatform,
+
+);
   final repository = MockDatabase();
   runApp(MyApp( repository: repository));
-  
 }
+
+// void main() async {
+  
+//   final repository = MockDatabase();
+//   runApp(MyApp( repository: repository));
+  
+// }
 class MyApp extends StatelessWidget {
  MyApp({super.key, required DatabaseRepository repository});
 
